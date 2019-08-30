41ec64bb8346a2e7966b5c5bf5ba56fe5cac45a1
Don <donp66@gmail.com>
2019-08-30 10:44:38 -0400

    Inital Build

---
+++ precache-manifest.07894d72ba1c02ad924c7fbc5fb29cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05af2c6c524cf1c7c86964036b3456b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f3e3f3a948d2fc58148",
+    "url": "/static/css/main.bb4f42c2.chunk.css"
+  },
+  {
+    "revision": "6ba70591784f53dcdedc",
+    "url": "/static/js/2.19a5dbf5.chunk.js"
+  },
+  {
+    "revision": "3f3e3f3a948d2fc58148",
+    "url": "/static/js/main.fad812b4.chunk.js"
+  },
+  {
+    "revision": "c85ea4da173a698fc1f7",
+    "url": "/static/js/runtime~main.66f789b0.js"
+  },
+  {
+    "revision": "2f3478370f491a527cd2fa55bb640fff",
+    "url": "/static/media/MetalBg.2f347837.jxr"
+  }
+]);
